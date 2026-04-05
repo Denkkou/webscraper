05cc53d67bb7ca82eddc6c84be2f9001ea1e127c
Denkkou <JoeSchoDev@gmail.com>
2026-04-05 22:36:51 +0300

    stubbed getHTML function

---
+++ get_html.go
@@ -0,0 +1,18 @@
+package main 
+
+import (
+
+)
+
+func getHTML(rawURL string) (string, error) {
+	// build http request to fetch page of rawURL
+
+	// return error if http status code is 400+
+
+	// return error if content header is not text/html
+
+	// return other errors
+
+	//return page's html as string
+	return "", nil
+}
